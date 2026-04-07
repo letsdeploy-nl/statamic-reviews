3876e38f70da2d226c98fa38b13fb05a51c367a2
Sem Bogaarts <sembogaarts@gmail.com>
2026-04-07 21:09:11 +0200

    :bug: fixed date conversion for new statamic

--- src/Repositories/ReviewRepository.php
+++ src/Repositories/ReviewRepository.php
@@ -11,44 +11,44 @@ class ReviewRepository
 {
     public function updateOrCreate(Reviewable $review)
     {
-        $entry = Entry::whereCollection('reviews')
-            ->where('review_id', $review->reviewId)
+        $entry = Entry::whereCollection("reviews")
+            ->where("review_id", $review->reviewId)
             ->first();
 
         if ($entry) {
-            $entry->set('title', $review->title);
-            $entry->set('content', $review->content);
-            $entry->set('stars', $review->stars);
-            $entry->set('raw_json', $review->rawJson);
+            $entry->set("title", $review->title);
+            $entry->set("content", $review->content);
+            $entry->set("stars", $review->stars);
+            $entry->set("raw_json", $review->rawJson);
             $entry->save();
             return;
-        };
+        }
 
         Entry::make()
-            ->collection('reviews')
+            ->collection("reviews")
             ->data([
-                'title' => $review->title,
-                'content' => $review->content,
-                'stars' => $review->stars,
-                'name' => $review->name,
-                'reviewer_photo_url' => $review->reviewerPhotoUrl,
-                'created_at' => Carbon::parse($review->createdAt),
-                'review_origin' => $review->reviewOrigin,
-                'review_id' => $review->reviewId,
-                'review_url' => $review->reviewUrl,
-                'review_image_urls' => $review->reviewImageUrls->toArray(),
-                'raw_json' => $review->rawJson,
+                "title" => $review->title,
+                "content" => $review->content,
+                "stars" => $review->stars,
+                "name" => $review->name,
+                "reviewer_photo_url" => $review->reviewerPhotoUrl,
+                "created_at" => $review->createdAt->timestamp,
+                "review_origin" => $review->reviewOrigin,
+                "review_id" => $review->reviewId,
+                "review_url" => $review->reviewUrl,
+                "review_image_urls" => $review->reviewImageUrls->toArray(),
+                "raw_json" => $review->rawJson,
             ])
             ->save();
     }
 
     public function updateGlobal(Reviewable $review)
     {
-        $set = GlobalSet::findByHandle('review');
+        $set = GlobalSet::findByHandle("review");
         $variables = $set->inDefaultSite();
         $variables->data([
-            'total_score' => $review->totalScore,
-            'reviews_count' => $review->reviewsCount
+            "total_score" => $review->totalScore,
+            "reviews_count" => $review->reviewsCount,
         ]);
         $variables->save();
     }
